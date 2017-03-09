b441f4d8f7024784d1e6c53bd9998874a956abe1
aminov <rustam.aminov@sibers.com>
2017-03-09 14:44:34 +0600

    - Add bonus cards

--- src/components/table.js
+++ src/components/table.js
@@ -6,15 +6,12 @@ class Table extends Component {
      * @returns {XML}
      */
     render() {
-        const cards = this.props.cards.startGame;
-        const singleCard = cards.map(function(item) {
-            const lvlSign = 'Уровень ';
-            const lvlUpSign = ' Уровня ';
-            const treasureSign = ' Сокровища ';
+        const doorCards = this.props.cards.startGame.doors;
+        const doors = doorCards.map(function(item) {
             let curse = (item.curse == true) ? 'Проклятие!' : '';
-            let lvl = (item.lvlMstr == 0) ? '' : lvlSign + item.lvlMstr;
-            let lvlUp = (item.lvlUp == 0) ? '' : item.lvlUp + lvlUpSign;
-            let treasure = (item.treasure == 0) ? '' : item.treasure + treasureSign;
+            let lvl = (item.lvlMstr == 0) ? '' : 'Уровень ' + item.lvlMstr;
+            let lvlUp = (item.lvlUp == 0) ? '' : item.lvlUp + ' Уровня';
+            let treasure = (item.treasure == 0) ? '' : item.treasure + ' Сокровища ';
             return (
                     <div className='col-md-3 singleCard' key={item.id}>
                         <h4 className='text-center'>{curse}</h4>
@@ -27,8 +24,24 @@ class Table extends Component {
                     </div>
             );
         });
+        const treasureCards = this.props.cards.startGame.treasures;
+        const treasures = treasureCards.map(function(item) {
+            let bonus = (item.bonus == 0) ? '' : '+' + item.bonus + ' Бонус';
+            let gold = (item.golds == 0) ? '' : item.golds + ' голдов';
+          return(
+              <div className='col-md-3 singleCard' key={item.id}>
+                  <h4 className='text-center'>{bonus}</h4>
+                  <p className='text-center'>{item.useOnly}</p>
+                  <h4 className='text-center'>{item.name}</h4>
+                  <img src='/static/src/img/img.jpg' alt={item.name}/>
+                  <p>{item.description}</p>
+                  <div className='text-left' id='left'>{item.type}</div>
+                  <div className='text-right' id='right'>{gold}</div>
+              </div>
+          );
+        });
         return (
-                <div className='blabla'>{singleCard}</div>
+                <div className='blabla'>{doors}{treasures}</div>
         )
     }
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 export const START_GAME = 'START_GAME';
 
-export const initialState = [
+export const initialState = {
+    doors: [
         {
             id: 0,
             treasure: 0,
@@ -48,5 +49,89 @@ export const initialState = [
             disaster: 'blablabla',
             name: 'Кальмадзилла',
             description: 'Склизко! Эльфы в -4! Не преследует никого, чей Уровень 4 или ниже, за исключением Эльфов. Непотребство: хватает, сдавливает противными щупальцами и пожирает. Ты мертв. Вопросы, пожелания, высказывания?'
+        },
+        {
+            id: 4,
+            treasure: 0,
+            curse: true,
+            lvlUp: 0,
+            lvlMstr: 0,
+            race: '',
+            class: '',
+            disaster: 'blablabla',
+            name: '',
+            description: 'Теряешь надетую Обувку.'
+        },
+        {
+            id: 5,
+            treasure: 0,
+            curse: false,
+            lvlUp: 0,
+            lvlMstr: 0,
+            race: '',
+            class: '',
+            disaster: 'blablabla',
+            name: 'Дварф',
+            description: 'Можешь надеть любое число Больших шмоток. Можешь держать в <<руке>> 6 карт'
+        },
+        {
+            id: 6,
+            treasure: 0,
+            curse: false,
+            lvlUp: 0,
+            lvlMstr: 0,
+            race: '',
+            class: '',
+            disaster: 'blablabla',
+            name: 'Клирик',
+            description: 'Воскрешение: когда надо вытянуть карту лицом вверх, ты можешь вместо этого взять верхнюю карту из соответствующей кучи сброса. Затем ты должен сбрросить одну карту с <<руки>> Изгнание: можешь сбросить до 3х карт в бою против Андедов. Каждый сброс дает тебе +3 Бонус.'
         }
-];
+    ],
+    treasures: [
+        {
+            id: 0,
+            bonus: 0,
+            useOnly: '',
+            name: 'Меджик Мисайл!',
+            description: 'Применить в любом бою. Дает +5 любой стороне. Применить только один раз',
+            type: '',
+            golds: 300
+        },
+        {
+            id: 1,
+            bonus: 1,
+            useOnly: '',
+            name: 'Шлем Бесстрашия',
+            description: '',
+            type: 'Головняк',
+            golds: 200
+        },
+        {
+            id: 2,
+            bonus: 1,
+            useOnly: '',
+            name: 'Кожаный Прикид',
+            description: '',
+            type: 'Броник',
+            golds: 200
+        },
+        {
+            id: 3,
+            bonus: 3,
+            useOnly: '',
+            name: 'Бензопила Кровавого Расчленения',
+            description: '',
+            type: '2 Руки Большая',
+            golds: 600
+        },
+        {
+            id: 4,
+            bonus: 5,
+            useOnly: 'Применяется только Волшебником',
+            name: 'Посох Напалма',
+            description: '',
+            type: '1 Рука',
+            golds: 800
+        }
+    ]
+};
