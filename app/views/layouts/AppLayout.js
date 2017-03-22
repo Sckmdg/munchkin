acd9a5e0677c5ec3e5a5be3c1d479d3745b93af2
Vitaly Kochetkov <vitaly.kochetkov@sibers.com>
2017-03-22 12:42:35 +0600

    added paper element

--- app/views/layouts/AppLayout.js
+++ app/views/layouts/AppLayout.js
@@ -1,10 +1,18 @@
 import React, { PropTypes } from 'react';
+import Paper from 'material-ui/Paper';
 
 function AppLayout ({ children }) {
+  const style = {
+    height: 1000,
+    width: '95%',
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block'
+  };
   return (
-    <div>
+    <Paper style={style} zDepth={5}>
       {children}
-    </div>
+    </Paper>
   )
 }
 
